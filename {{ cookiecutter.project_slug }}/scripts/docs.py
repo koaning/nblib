37e7b3c4e27978931d5575d2249801c7f347f646
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-08-31 15:59:27 +0200

    update

--- {{ cookiecutter.project_slug }}/scripts/docs.py
+++ {{ cookiecutter.project_slug }}/scripts/docs.py
@@ -1,29 +1,6 @@
 import nbformat
 from pathlib import Path
 
-
-def convert_notebooks_to_python(input_folder, output_folder):
-    folder = Path(input_folder)
-    
-    # Iterate over all files in the specified folder
-    for notebook_path in folder.glob("*.ipynb"):
-        new_root_folder = Path(output_folder)
-        python_file_path = new_root_folder / notebook_path.with_suffix('.py').parts[-1]
-        
-        # Read the notebook
-        with notebook_path.open('r', encoding='utf-8') as nb_file:
-            nb_content = nbformat.read(nb_file, as_version=4)
-        
-        # Extract code cells
-        code_cells = [cell['source'] for cell in nb_content['cells'] if cell['cell_type'] == 'code']
-        
-        # Write the code cells to a Python file
-        with python_file_path.open('w', encoding='utf-8') as py_file:
-            for cell in code_cells:
-                if "## EXPORT" in cell:
-                    py_file.write(cell + '\n\n')
-
-
 def convert_notebook_to_docs(input_folder, output_folder):
     folder = Path(input_folder)
     # Iterate over all files in the specified folder
@@ -59,7 +36,6 @@ def convert_notebook_to_docs(input_folder, output_folder):
                                         md_file.write(f'\n<img src="data:image/png;base64, {img_repr}"/>\n')
 
 if __name__ == "__main__":
-    convert_notebooks_to_python("src", "{{ cookiecutter.project_slug }}")
     convert_notebook_to_docs(".tmpdir", "docs")
     Path("docs/__init__.md").rename("docs/index.md")
     Path("README.md").write_text(Path("docs/index.md").read_text())
---
+++ {{ cookiecutter.project_slug }}/scripts/src.py
@@ -0,0 +1,27 @@
+import nbformat
+from pathlib import Path
+
+
+def convert_notebooks_to_python(input_folder, output_folder):
+    folder = Path(input_folder)
+    
+    # Iterate over all files in the specified folder
+    for notebook_path in folder.glob("*.ipynb"):
+        new_root_folder = Path(output_folder)
+        python_file_path = new_root_folder / notebook_path.with_suffix('.py').parts[-1]
+        
+        # Read the notebook
+        with notebook_path.open('r', encoding='utf-8') as nb_file:
+            nb_content = nbformat.read(nb_file, as_version=4)
+        
+        # Extract code cells
+        code_cells = [cell['source'] for cell in nb_content['cells'] if cell['cell_type'] == 'code']
+        
+        # Write the code cells to a Python file
+        with python_file_path.open('w', encoding='utf-8') as py_file:
+            for cell in code_cells:
+                if "## EXPORT" in cell:
+                    py_file.write(cell + '\n\n')
+
+if __name__ == "__main__":
+    convert_notebooks_to_python("src", "proj")
